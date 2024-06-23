6afbd5208ce639f77775e1a1647dbdbf47440644
not-ed <43544402+not-ed@users.noreply.github.com>
2024-06-23 10:51:26 +0100

    Adjusted TestingSqliteConnectionFactory to provide sharable in-memory connections.
    To carry out Unit Tests which rely on SQLite, we use an in-memory database to perform operations on, which is deleted when the connection made to it is closed (https://www.sqlite.org/inmemorydb.html
    https://learn.microsoft.com/en-us/dotnet/standard/data/sqlite/in-memory-databases). This is fine for testing one-off operations, as they are not dependent on the state of the database or its existing contents, but would become problematic once
    other operations need to be ran as part of testing setup, as a method which closes a connection would inadvertantly wipe the in-memory database. To prevent this, TestingSqliteConnectionFactory has been adjusted to maintain a connection that is
    shared with other test subjects that use it throughout its lifetime, which should prevent this.

--- Acai/AcaiCore/TestingSqliteConnectionFactory.cs
+++ Acai/AcaiCore/TestingSqliteConnectionFactory.cs
@@ -1,19 +1,37 @@
 ﻿using Microsoft.Data.Sqlite;
+using System.Data;
 
 namespace AcaiCore
 {
     public class TestingSqliteConnectionFactory : ISqliteConnectionFactory
     {
-        public SqliteConnection CreateOpenConnection()
-        {
-            var connection = new SqliteConnection("Data Source=:memory:");
-            connection.Open();
+        private SqliteConnection _connection;
+        private string _connectionString = "";
 
-            var createFoodItemsTableCommand = connection.CreateCommand();
+        public TestingSqliteConnectionFactory()
+        {
+            _connectionString = $"Data Source={Guid.NewGuid()};Mode=Memory;Cache=Shared";
+            _connection = new SqliteConnection(_connectionString);
+            _connection.Open();
+
+            var createFoodItemsTableCommand = _connection.CreateCommand();
             createFoodItemsTableCommand.CommandText = "CREATE TABLE IF NOT EXISTS \"food_items\" (\r\n\t\"id\"\tINTEGER NOT NULL UNIQUE,\r\n\t\"name\"\tTEXT NOT NULL,\r\n\t\"calories\"\tREAL NOT NULL,\r\n\t\"created_at\"\tTEXT NOT NULL,\r\n\tPRIMARY KEY(\"id\" AUTOINCREMENT)\r\n);";
             createFoodItemsTableCommand.ExecuteNonQuery();
             createFoodItemsTableCommand.Dispose();
+        }
 
+        ~TestingSqliteConnectionFactory()
+        {
+            if ( _connection.State != ConnectionState.Closed)
+            {
+                _connection.Close();
+            }
+        }
+
+        public SqliteConnection CreateOpenConnection()
+        {
+            var connection = new SqliteConnection(_connectionString);
+            connection.Open();
             return connection;
         }
     }
