b6a6ce24dc0ce9edff22b882a8e88d7b36d0d0a2
not-ed <43544402+not-ed@users.noreply.github.com>
2024-08-12 07:45:23 +0100

    Added new .NET MAUI project.

---
+++ Acai/AcaiMobile/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AcaiMobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
