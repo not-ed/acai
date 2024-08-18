c894e7f4e2404a9d8939acbc0b73fae5490467b8
not-ed <43544402+not-ed@users.noreply.github.com>
2024-08-18 18:52:10 +0100

    Added ObservableCollection and test hard-coded values for updating the display of saved journal Item entries

--- Acai/AcaiMobile/MainPage.xaml.cs
+++ Acai/AcaiMobile/MainPage.xaml.cs
@@ -1,15 +1,51 @@
-﻿namespace AcaiMobile
+﻿using System.Collections.ObjectModel;
+using AcaiCore;
+
+namespace AcaiMobile
 {
+    public class FoodItemViewData
+    {
+        public string name {get; set;}
+        public float calories {get; set;}
+        public DateTime creationDate { get; set;}
+
+        public FoodItemViewData(FoodItemDTO foodItem)
+        {
+            this.name = foodItem.GetName();
+            this.calories = foodItem.GetCalories();
+            this.creationDate = foodItem.GetCreationDate();
+        }
+
+        public FoodItemViewData(string name, float calories, DateTime creationDate)
+        {
+            this.name = name;
+            this.calories = calories;
+            this.creationDate = creationDate;
+        }
+    }
     public partial class MainPage : ContentPage
     {
+        private ObservableCollection<FoodItemViewData> _foodItems = new ObservableCollection<FoodItemViewData>();
+
         public MainPage()
         {
             InitializeComponent();
+            ItemListView.ItemsSource = _foodItems;
         }
 
-        private void OnAddItemButtonClicked(object sender, EventArgs e)
+        private void OnPageLoad(object sender, EventArgs e)
         {
-            Navigation.PushModalAsync(new NewItemContentPage());
+            for (int i = 0; i < 5; i++)
+            {
+                _foodItems.Add(new FoodItemViewData(new FoodItemDTO($"Test item {i}", i * 100, DateTime.Now)));
+            }
+        }
+
+        private async void OnAddItemButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new NewItemContentPage());
+
+            _foodItems.Add(new FoodItemViewData(new FoodItemDTO($"Test item {_foodItems.Count}", _foodItems.Count * 100, DateTime.Now)));
         }
     }
 }
