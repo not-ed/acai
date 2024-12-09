ff32d8be775c3ccc09b8136ad4ff864a27f37bea
not-ed <43544402+not-ed@users.noreply.github.com>
2024-12-09 21:17:21 +0000

    Created new FoodItemShortcutGateway interface (and associated FoodItemShortcutDTO Class)

---
+++ Acai/AcaiCore/FoodItemShortcutGateway/IFoodItemShortcutGateway.cs
@@ -0,0 +1,22 @@
+﻿namespace AcaiCore
+{
+    public interface IFoodItemShortcutGateway
+    {
+        public FoodItemShortcutDTO CreateNewFoodItemShortcut(string name, float calories);
+
+        public void DeleteFoodItemShortcut(long id);
+
+        public List<FoodItemShortcutDTO> GetAllFoodItemShortcuts();
+    }
+
+    public class FoodItemShortcutDTO
+    {
+        private long id;
+        private string name;
+        private float calories;
+
+        public long GetID() { return id; }
+        public string GetName() { return name; }
+        public float GetCalories() { return calories; }
+    }
+}
