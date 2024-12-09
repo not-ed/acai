aa5f452f27839f01d9ad57e24ae43b4bb963e818
not-ed <43544402+not-ed@users.noreply.github.com>
2024-12-09 21:48:17 +0000

    Created concrete FoodItemShortcutGateway class

---
+++ Acai/AcaiCore/FoodItemShortcutGateway/FoodItemShortcutGateway.cs
@@ -0,0 +1,26 @@
+﻿namespace AcaiCore;
+
+public class FoodItemShortcutGateway : IFoodItemShortcutGateway
+{
+    private ISqliteConnectionFactory sqliteConnectionFactory;
+
+    public FoodItemShortcutGateway(ISqliteConnectionFactory connectionFactory)
+    {
+        this.sqliteConnectionFactory = connectionFactory;
+    }
+
+    public FoodItemShortcutDTO CreateNewFoodItemShortcut(string name, float calories)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteFoodItemShortcut(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<FoodItemShortcutDTO> GetAllFoodItemShortcuts()
+    {
+        throw new NotImplementedException();
+    }
+}
