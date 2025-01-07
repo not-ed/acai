7015372c6524a443226eacc3ff4e17b216ede455
not-ed <43544402+not-ed@users.noreply.github.com>
2025-01-07 12:57:25 +0000

    Added population and selection of Food Item Shortcuts in New Food Item page

--- Acai/AcaiMobile/NewItemViewModel.cs
+++ Acai/AcaiMobile/NewItemViewModel.cs
@@ -1,7 +1,15 @@
+using AcaiCore;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
 namespace AcaiMobile;
+
+public partial class FoodItemViewShortcut(FoodItemShortcutDTO shortcut) : ObservableObject
+{
+    [ObservableProperty] private long _id = shortcut.GetID();
+    [ObservableProperty] private string _name = shortcut.GetName();
+    [ObservableProperty] private float _calories = shortcut.GetCalories();
+}
 
 public partial class NewItemViewModel : ObservableObject
 {
@@ -17,7 +25,22 @@ public partial class NewItemViewModel : ObservableObject
     private DateTime _newItemCreationDate = DateTime.Now;
     [ObservableProperty]
     private bool _createNewFoodItemShortcut = false;
+    
+    [ObservableProperty]
+    private List<FoodItemViewShortcut> _foodItemShortcuts;
 
+    public NewItemViewModel()
+    {
+        var session = AcaiSessionSingleton.Get(null).Result;
+        _foodItemShortcuts = new List<FoodItemViewShortcut>();
+        foreach (var shortcut in session.GetFoodItemShortcutGateway().GetAllFoodItemShortcuts())
+        {
+            _foodItemShortcuts.Add(new FoodItemViewShortcut(shortcut));
+        }
+
+        _foodItemShortcuts = _foodItemShortcuts.OrderBy(x => x.Name).ToList();
+    }
+    
     [RelayCommand]
     private void ValidateNewItemDetails()
     {
@@ -32,5 +55,14 @@ public partial class NewItemViewModel : ObservableObject
     {
         _submitted = true;
         Shell.Current.Navigation.PopModalAsync(true);
+    }
+
+    [RelayCommand]
+    private void PopulateFieldsWithFoodItemShortcut(FoodItemViewShortcut shortcut)
+    {
+        NewItemName = shortcut.Name;
+        NewItemCalories = shortcut.Calories;
+        
+        CreateNewFoodItemShortcut = false;
     }
 }
