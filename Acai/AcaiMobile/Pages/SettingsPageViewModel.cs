a5eafe9f206bb4fe27c370fdd29aabfa8cc596f3
not-ed <43544402+not-ed@users.noreply.github.com>
2025-01-27 22:38:38 +0000

    Added SettingsPageViewModel logic for updating Caloric Limit preferences

--- Acai/AcaiMobile/Pages/SettingsPageViewModel.cs
+++ Acai/AcaiMobile/Pages/SettingsPageViewModel.cs
@@ -1,14 +1,23 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace AcaiMobile.Pages;
 
 public partial class SettingsPageViewModel : ObservableObject
 {
     [ObservableProperty]
-    private float _dailyCaloricLimit = 1234;
+    private float _dailyCaloricLimit = Preferences.Get(DailyCaloricLimitPreferencesKey, DailyCaloricLimitDefaultValue);
+    private const string DailyCaloricLimitPreferencesKey = "dailyCaloricLimit";
+    private const float DailyCaloricLimitDefaultValue = 2000.0f;
 
-    public SettingsPageViewModel()
+    [RelayCommand]
+    private async void UpdateDailyCaloricLimitSetting()
     {
-        
+        var newDailyCaloricLimitPrompt = await Shell.Current.DisplayPromptAsync("Daily Caloric Limit", string.Empty, "Submit", "Cancel", "2000 kcal",-1, Keyboard.Numeric, DailyCaloricLimit.ToString());
+        if (float.TryParse(newDailyCaloricLimitPrompt, out var enteredLimit) && enteredLimit != DailyCaloricLimit)
+        {
+            Preferences.Set(DailyCaloricLimitPreferencesKey, enteredLimit);
+            DailyCaloricLimit = Preferences.Get(DailyCaloricLimitPreferencesKey, DailyCaloricLimitDefaultValue);
+        }
     }
 }
