30e42c73dee24f325f6e834c90d08c0c5740f0c6
not-ed <43544402+not-ed@users.noreply.github.com>
2025-02-09 14:27:51 +0000

    Refactor: Adjusted PresentInConnection method in FoodItemShortcutTableSchema to use PRAGMA queries
    Because we want to extend the food_item_shortcuts table to include new columns using a new IJournalTableSchema, the current approach of checking for the exact creation query used in sqlite_master no longer feels sustainable without having to retroactively change old schemas when new ones are introduced, as altering a table will also alter the creation query returned.
    
    In response to this, PRAGMA (TABLE_INFO) queries will now be used to validate the presence of all required columns in the schema, the idea being that more granular control can be given to ensuring that only the relevant columns introduced by a schema can be validated.

--- Acai/AcaiCore/JournalTableSchemas/FoodItemShortcutTableSchema.cs
+++ Acai/AcaiCore/JournalTableSchemas/FoodItemShortcutTableSchema.cs
@@ -12,16 +12,71 @@ namespace AcaiCore
 
         public bool PresentInConnection(SqliteConnection sqliteConnection)
         {
-            bool schemaIsPresentInConnection = false;
+            var idColumnIsPresent = false;
+            var nameColumnIsPresent = false;
+            var caloriesColumnIsPresent = false;
+            
             using (var selectTableCommand = sqliteConnection.CreateCommand())
             {
-                selectTableCommand.CommandText = $"SELECT * FROM sqlite_master where name = 'food_item_shortcuts' AND sql = '{creationQuery}' LIMIT 1;";
+                selectTableCommand.CommandText = "PRAGMA TABLE_INFO(food_item_shortcuts);";
                 using (var reader = selectTableCommand.ExecuteReader())
                 {
-                    schemaIsPresentInConnection = reader.Read();
+                    while (reader.Read())
+                    {
+                        if (!idColumnIsPresent && IDColumnIsPresentInSchema(reader))
+                        {
+                            idColumnIsPresent = true;
+                            continue;
+                        }
+                        
+                        if (!nameColumnIsPresent && NameColumnIsPresentInSchema(reader))
+                        {
+                            nameColumnIsPresent = true;
+                            continue;
+                        }
+                        
+                        if (!caloriesColumnIsPresent && CaloriesColumnIsPresentInSchema(reader))
+                        {
+                            caloriesColumnIsPresent = true;
+                        }
+                    }
                 }
             }
-            return schemaIsPresentInConnection;
+
+            return idColumnIsPresent && nameColumnIsPresent && caloriesColumnIsPresent;
+        }
+        
+        private bool IDColumnIsPresentInSchema(SqliteDataReader reader)
+        {
+            if (reader.GetInt64(0) != 0) return false; // cid
+            if (reader.GetString(1) != "id") return false; // name
+            if (reader.GetString(2) != "INTEGER") return false; // type
+            if (reader.GetInt64(3) != 1) return false; // notnull
+            if (reader.GetInt64(5) != 1) return false; // pk
+
+            return true;
+        }
+        
+        private bool NameColumnIsPresentInSchema(SqliteDataReader reader)
+        {
+            if (reader.GetInt64(0) != 1) return false; // cid
+            if (reader.GetString(1) != "name") return false; // name
+            if (reader.GetString(2) != "TEXT") return false; // type
+            if (reader.GetInt64(3) != 1) return false; // notnull
+            if (reader.GetInt64(5) != 0) return false; // pk
+
+            return true;
+        }
+        
+        private bool CaloriesColumnIsPresentInSchema(SqliteDataReader reader)
+        {
+            if (reader.GetInt64(0) != 2) return false; // cid
+            if (reader.GetString(1) != "calories") return false; // name
+            if (reader.GetString(2) != "REAL") return false; // type
+            if (reader.GetInt64(3) != 1) return false; // notnull
+            if (reader.GetInt64(5) != 0) return false; // pk
+
+            return true;
         }
     }
 }
