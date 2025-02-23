0205ca8427d16896edd450eeb7219bc6cf22d6ce
not-ed <43544402+not-ed@users.noreply.github.com>
2025-02-23 21:09:47 +0000

    Adding PreferenceKeyDefaultValuePairs for upcoming/remaining macronutrients

--- Acai/AcaiMobile/PreferenceIndex.cs
+++ Acai/AcaiMobile/PreferenceIndex.cs
@@ -10,4 +10,8 @@ public static class PreferenceIndex
 {
     public static readonly PreferencesKeyDefaultValuePair<float> DailyCaloricLimit = new("dailyCaloricLimit", 2000.0f);
     public static readonly PreferencesKeyDefaultValuePair<bool> DisplayProtein = new("displayProtein", true);
+    public static readonly PreferencesKeyDefaultValuePair<bool> DisplayCarbohydrates = new("displayCarbohydrates", true);
+    public static readonly PreferencesKeyDefaultValuePair<bool> DisplayFat = new("displayFat", true);
+    public static readonly PreferencesKeyDefaultValuePair<bool> DisplayFibre = new("displayFibre", true);
+    public static readonly PreferencesKeyDefaultValuePair<bool> DisplayWater = new("displayWater", true);
 }
