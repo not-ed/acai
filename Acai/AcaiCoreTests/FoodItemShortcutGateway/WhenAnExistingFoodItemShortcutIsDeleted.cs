967ddd5364cddd3f3eb1fc8841bb7e2e862ab849
not-ed <43544402+not-ed@users.noreply.github.com>
2025-02-23 19:02:35 +0000

    Updated unit tests for DeleteFoodItemShortcut to include macronutrient properties

--- Acai/AcaiCoreTests/FoodItemShortcutGateway/WhenAnExistingFoodItemShortcutIsDeleted.cs
+++ Acai/AcaiCoreTests/FoodItemShortcutGateway/WhenAnExistingFoodItemShortcutIsDeleted.cs
@@ -20,11 +20,14 @@ public class WhenAnExistingFoodItemShortcutIsDeleted
                 command.CommandText = new FoodItemShortcutTableSchema().GetSQLTableCreationQuery();
                 command.ExecuteNonQuery();
 
-                command.CommandText = "INSERT INTO food_item_shortcuts (id, name, calories) VALUES" +
-                                      "(1, 'Test Shortcut 1', 100)," +
-                                      "(2, 'Test Shortcut 2', 200)," +
-                                      "(3, 'Test Shortcut 3', 300)," +
-                                      "(4, 'Test Shortcut 4', 400);";
+                command.CommandText = new FoodItemShortcutMacronutrientsSchema().GetSQLTableCreationQuery();
+                command.ExecuteNonQuery();
+                
+                command.CommandText = "INSERT INTO food_item_shortcuts (id, name, calories, protein, carbohydrates, fat, fibre, water) VALUES" +
+                                      "(1, 'Test Shortcut 1', 100, 10, 20, 30, 40, 50)," +
+                                      "(2, 'Test Shortcut 2', 200, 60, 70, 80, 90, 100)," +
+                                      "(3, 'Test Shortcut 3', 300, 110, 120, 130, 140, 150)," +
+                                      "(4, 'Test Shortcut 4', 400, 160, 170, 180, 190, 200);";
                 command.ExecuteNonQuery();
             }
         }
@@ -37,12 +40,20 @@ public class WhenAnExistingFoodItemShortcutIsDeleted
         {
             using (var command = connection.CreateCommand())
             {
-                command.CommandText = "SELECT id, name, calories FROM food_item_shortcuts;";
+                command.CommandText = "SELECT id, name, calories, protein, carbohydrates, fat, fibre, water FROM food_item_shortcuts;";
                 using (var reader = command.ExecuteReader())
                 {
                     while (reader.Read())
                     {
-                        _result.Add(new FoodItemShortcutDTO(reader.GetInt64(0),reader.GetString(1),reader.GetFloat(2), null, null, null, null, null));
+                        _result.Add(new FoodItemShortcutDTO(
+                            reader.GetInt64(0),
+                            reader.GetString(1),
+                            reader.GetFloat(2),
+                            reader.GetFloat(3),
+                            reader.GetFloat(4),
+                            reader.GetFloat(5),
+                            reader.GetFloat(6),
+                            reader.GetFloat(7)));
                     }
                 }
             }
@@ -62,16 +73,40 @@ public class WhenAnExistingFoodItemShortcutIsDeleted
         Assert.That(_result.Count, Is.EqualTo(3));
 
         var firstRemainingFoodItem = _result.First(x => x.GetID() == 1);
-        Assert.That(firstRemainingFoodItem.GetName(), Is.EqualTo("Test Shortcut 1"));
-        Assert.That(firstRemainingFoodItem.GetCalories(), Is.EqualTo(100));
+        Assert.Multiple(() =>
+        {
+            Assert.That(firstRemainingFoodItem.GetName(), Is.EqualTo("Test Shortcut 1"));
+            Assert.That(firstRemainingFoodItem.GetCalories(), Is.EqualTo(100));
+            Assert.That(firstRemainingFoodItem.GetProtein(), Is.EqualTo(10));
+            Assert.That(firstRemainingFoodItem.GetCarbohydrates(), Is.EqualTo(20));
+            Assert.That(firstRemainingFoodItem.GetFat(), Is.EqualTo(30));
+            Assert.That(firstRemainingFoodItem.GetFibre(), Is.EqualTo(40));
+            Assert.That(firstRemainingFoodItem.GetWater(), Is.EqualTo(50));
+        });
         
         var secondRemainingFoodItem = _result.First(x => x.GetID() == 3);
-        Assert.That(secondRemainingFoodItem.GetName(), Is.EqualTo("Test Shortcut 3"));
-        Assert.That(secondRemainingFoodItem.GetCalories(), Is.EqualTo(300));
-
+        Assert.Multiple(() =>
+        {
+            Assert.That(secondRemainingFoodItem.GetName(), Is.EqualTo("Test Shortcut 3"));
+            Assert.That(secondRemainingFoodItem.GetCalories(), Is.EqualTo(300));
+            Assert.That(secondRemainingFoodItem.GetProtein(), Is.EqualTo(110));
+            Assert.That(secondRemainingFoodItem.GetCarbohydrates(), Is.EqualTo(120));
+            Assert.That(secondRemainingFoodItem.GetFat(), Is.EqualTo(130));
+            Assert.That(secondRemainingFoodItem.GetFibre(), Is.EqualTo(140));
+            Assert.That(secondRemainingFoodItem.GetWater(), Is.EqualTo(150));
+        });
+        
         var thirdRemainingFoodItem = _result.First(x => x.GetID() == 4);
-        Assert.That(thirdRemainingFoodItem.GetName(), Is.EqualTo("Test Shortcut 4"));
-        Assert.That(thirdRemainingFoodItem.GetCalories(), Is.EqualTo(400));
+        Assert.Multiple(() =>
+        {
+            Assert.That(thirdRemainingFoodItem.GetName(), Is.EqualTo("Test Shortcut 4"));
+            Assert.That(thirdRemainingFoodItem.GetCalories(), Is.EqualTo(400));
+            Assert.That(thirdRemainingFoodItem.GetProtein(), Is.EqualTo(160));
+            Assert.That(thirdRemainingFoodItem.GetCarbohydrates(), Is.EqualTo(170));
+            Assert.That(thirdRemainingFoodItem.GetFat(), Is.EqualTo(180));
+            Assert.That(thirdRemainingFoodItem.GetFibre(), Is.EqualTo(190));
+            Assert.That(thirdRemainingFoodItem.GetWater(), Is.EqualTo(200));
+        });
     }
     
 }
