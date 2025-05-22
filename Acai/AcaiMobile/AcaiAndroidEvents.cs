5bf6bdd58211260348b32c9cc3d6c31cccca12d4
not-ed <43544402+not-ed@users.noreply.github.com>
2025-05-22 22:37:18 +0100

    Added stub OnResume Method as a lifecycle event when app is resumed.

---
+++ Acai/AcaiMobile/AcaiAndroidEvents.cs
@@ -0,0 +1,12 @@
+﻿using CommunityToolkit.Maui.Alerts;
+
+namespace AcaiMobile;
+
+public static class AcaiAndroidEvents
+{
+    public static void OnResume()
+    {
+        var placeholderToast = Toast.Make("TODO: Check for Updates.");
+        placeholderToast.Show();
+    }
+}
--- Acai/AcaiMobile/MauiProgram.cs
+++ Acai/AcaiMobile/MauiProgram.cs
@@ -1,6 +1,7 @@
 ﻿using AcaiMobile.Pages;
 using CommunityToolkit.Maui;
 using CommunityToolkit.Maui.Core;
+using Microsoft.Maui.LifecycleEvents;
 
 namespace AcaiMobile
 {
@@ -17,7 +18,12 @@ namespace AcaiMobile
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+                })
+                .ConfigureLifecycleEvents(events =>
+                    events.AddAndroid(android => 
+                        android.OnResume(_ => AcaiAndroidEvents.OnResume())
+                    )
+                );
 
             // Setup dependency injection for injecting ViewModel class as the binding context of a page.
             builder.Services.AddSingleton<FoodJournalPage>();
