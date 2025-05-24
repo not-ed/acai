577f67554471aeacd527c2cb82ef8f92bc3fb126
not-ed <43544402+not-ed@users.noreply.github.com>
2025-05-24 18:22:28 +0100

    Added invocation of AcaiUpdateChecker upon Resuming app

--- Acai/AcaiMobile/AcaiAndroidEvents.cs
+++ Acai/AcaiMobile/AcaiAndroidEvents.cs
@@ -1,12 +1,9 @@
-﻿using CommunityToolkit.Maui.Alerts;
-
-namespace AcaiMobile;
+﻿namespace AcaiMobile;
 
 public static class AcaiAndroidEvents
 {
     public static void OnResume()
     {
-        var placeholderToast = Toast.Make("TODO: Check for Updates.");
-        placeholderToast.Show();
+        AcaiUpdateChecker.CheckForUpdates();
     }
 }
