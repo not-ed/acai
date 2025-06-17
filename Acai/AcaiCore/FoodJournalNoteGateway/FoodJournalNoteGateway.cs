a4b71752a161ddca3d44539c6f78ae60990c32d4
not-ed <43544402+not-ed@users.noreply.github.com>
2025-06-17 23:00:15 +0100

    Added logic and tests for retrieving a Food Journal Note by date

---
+++ Acai/AcaiCore/FoodJournalNoteGateway/FoodJournalNoteGateway.cs
@@ -0,0 +1,42 @@
+﻿namespace AcaiCore;
+
+public class FoodJournalNoteGateway : IFoodJournalNoteGateway
+{
+    private ISqliteConnectionFactory sqliteConnectionFactory;
+
+    public FoodJournalNoteGateway(ISqliteConnectionFactory connectionFactory)
+    {
+        this.sqliteConnectionFactory = connectionFactory;
+    }
+    
+    public FoodJournalNoteDTO CreateOrUpdateNoteForDate(DateTime date, string content)
+    {
+        throw new NotImplementedException();
+    }
+
+    public FoodJournalNoteDTO? GetNoteForDate(DateTime date)
+    {
+        FoodJournalNoteDTO? note = null;
+        
+        using (var connection = sqliteConnectionFactory.CreateOpenConnection())
+        {
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = $"SELECT id, date, content FROM food_journal_notes WHERE date(date) = date('{date.ToString("yyyy-MM-dd")}');";
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        long noteId = reader.GetInt64(0);
+                        DateTime noteDate = reader.GetDateTime(1);
+                        string noteContent = reader.GetString(2);
+                        
+                        note = new FoodJournalNoteDTO(noteId, noteDate, noteContent);
+                    }
+                }
+            }
+        }
+        
+        return note;
+    }
+}
---
+++ Acai/AcaiCoreTests/FoodJournalNoteGateway/WhenAFoodJournalNoteForAParticularDateIsRetrieved.cs
@@ -0,0 +1,51 @@
+﻿using AcaiCore;
+using NUnit.Framework;
+
+namespace AcaiCoreTests.FoodJournalNoteGateway;
+
+[TestFixture]
+public class WhenAFoodJournalNoteForAParticularDateIsRetrieved
+{
+    private FoodJournalNoteDTO? _result;
+
+    [OneTimeSetUp]
+    public void Setup()
+    {
+        var connectionFactory = new TestingSqliteConnectionFactory();
+        using (var connection = connectionFactory.CreateOpenConnection())
+        {
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = new FoodJournalNotesTableSchema().GetSQLTableCreationQuery();
+                command.ExecuteNonQuery();
+                
+                command.CommandText = "INSERT INTO food_journal_notes (id, date, content) VALUES " +
+                                      "(3, '2025-06-17', 'Test note content with **bold** and *italic* formatting.')";
+                command.ExecuteNonQuery();
+            }
+        }
+        
+        var subject = new AcaiCore.FoodJournalNoteGateway(connectionFactory);
+        _result = subject.GetNoteForDate(new DateTime(2025, 6, 17));
+    }
+
+    [Test]
+    public void ThenAnIDForTheNewItemIsReturned()
+    {
+        Assert.That(_result.GetID(), Is.EqualTo(3));
+    }
+    
+    [Test]
+    public void ThenTheCorrectContentIsReturned()
+    {
+        Assert.That(_result.GetContent(), Is.EqualTo("Test note content with **bold** and *italic* formatting."));
+    }
+
+    [Test]
+    public void ThenTheCorrectDateIsReturned()
+    {
+        Assert.That(_result.GetDate().Year, Is.EqualTo(2025));
+        Assert.That(_result.GetDate().Month, Is.EqualTo(6));
+        Assert.That(_result.GetDate().Day, Is.EqualTo(17));
+    }
+}
