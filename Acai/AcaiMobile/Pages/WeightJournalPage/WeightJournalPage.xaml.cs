295e0b529821ccff204af755f853708cdabc3efa
not-ed <43544402+not-ed@users.noreply.github.com>
2025-10-26 19:52:01 +0000

    Added DataTemplates and Command Stubs for entries to Weight Journal page

--- Acai/AcaiMobile/Pages/WeightJournalPage/WeightJournalPage.xaml.cs
+++ Acai/AcaiMobile/Pages/WeightJournalPage/WeightJournalPage.xaml.cs
@@ -1,15 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace AcaiMobile.Pages;
 
 public partial class WeightJournalPage : ContentPage
 {
-    public WeightJournalPage()
+    public WeightJournalPage(WeightJournalPageViewModel viewModel)
     {
         InitializeComponent();
+        
+        BindingContext = viewModel;
     }
 }
--- Acai/AcaiMobile/Pages/WeightJournalPage/WeightJournalPageViewModel.cs
+++ Acai/AcaiMobile/Pages/WeightJournalPage/WeightJournalPageViewModel.cs
@@ -1,8 +1,62 @@
+using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace AcaiMobile;
 
-public class WeightJournalPageViewModel : ObservableObject
+public partial class WeightJournalViewItem(DateTime creationDate, string displayWeight, float bodyFatPercentage) : ObservableObject
 {
+    [ObservableProperty] private DateTime _creationDate = creationDate;
+    [ObservableProperty] private string _displayWeight = displayWeight;
+    [ObservableProperty] private float _bodyFatPercentage = bodyFatPercentage;
+    [ObservableProperty] private string _note = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec magna id lorem sollicitudin gravida. Integer molestie euismod arcu, vel.";
+    [ObservableProperty] private bool _isExpanded = false;
+}
+
+public partial class WeightJournalPageViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private ObservableCollection<WeightJournalViewItem> _entryList = new ObservableCollection<WeightJournalViewItem>();
+
+    public WeightJournalPageViewModel()
+    {
+        ReinitializeEntriesList();
+    }
     
+    private async void ReinitializeEntriesList()
+    {
+        EntryList.Clear();
+        
+        EntryList.Add(new WeightJournalViewItem(DateTime.Now,"12st 9lbs", 30));
+        EntryList.Add(new WeightJournalViewItem(DateTime.Now.AddDays(-7),"80.2kg", 29.4f));
+        EntryList.Add(new WeightJournalViewItem(DateTime.Now.AddDays(-14),"177lbs", 29.6f));
+        EntryList.Add(new WeightJournalViewItem(DateTime.Now.AddDays(-21),"177lbs", 29.6f));
+        EntryList.Add(new WeightJournalViewItem(DateTime.Now.AddDays(-28),"177lbs", 29.6f));
+        EntryList.Add(new WeightJournalViewItem(DateTime.Now.AddDays(-35),"177lbs", 29.6f));
+        EntryList.Add(new WeightJournalViewItem(DateTime.Now.AddDays(-42),"177lbs", 29.6f));
+        EntryList.Add(new WeightJournalViewItem(DateTime.Now.AddDays(-49),"177lbs", 29.6f));
+    }
+    
+    [RelayCommand]
+    public void ToggleItemExpansion(WeightJournalViewItem selectedItem)
+    {
+        var selectedItemIsAlreadyExpanded = selectedItem.IsExpanded;
+        foreach (var entry in EntryList)
+        {
+            entry.IsExpanded = false;
+        }
+        selectedItem.IsExpanded = !selectedItemIsAlreadyExpanded;
+    }
+    
+    [RelayCommand]
+    public async void PromptItemDeletion(WeightJournalViewItem selectedItem)
+    {
+        var result = await Shell.Current.DisplayPromptAsync("STUB", $"Entry Deletion: {selectedItem.CreationDate}");
+    }
+    
+    [RelayCommand]
+    public async void EditItem(WeightJournalViewItem selectedItem)
+    {
+        var result = await Shell.Current.DisplayPromptAsync("STUB", $"Entry Edit: {selectedItem.CreationDate}");
+    }
 }
