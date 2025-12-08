61d5eb7b2e2c4b45874e7debcc7652b6b2c9ce35
not-ed <43544402+not-ed@users.noreply.github.com>
2025-12-08 19:56:43 +0000

    Implemented CreateNewWeighIn logic with tests

--- Acai/AcaiCore/WeightJournalGateway/WeightJournalGateway.cs
+++ Acai/AcaiCore/WeightJournalGateway/WeightJournalGateway.cs
@@ -1,13 +1,71 @@
-﻿namespace AcaiCore;
+﻿using Microsoft.Data.Sqlite;
+
+namespace AcaiCore;
 
 public class WeightJournalGateway : IWeightJournalGateway
 {
-    public WeightJournalEntryDTO CreateNewWeighIn(DateTime creationDate, float canonicalPounds, float? bodyFatPercentage, string note)
+    private ISqliteConnectionFactory _sqliteConnectionFactory;
+
+    public WeightJournalGateway(ISqliteConnectionFactory sqliteConnectionFactory)
     {
-        throw new NotImplementedException();
+        _sqliteConnectionFactory = sqliteConnectionFactory;
+    }
+    
+    public WeightJournalEntryDTO CreateNewWeighIn(DateTime creationDate, float canonicalPounds, float? bodyFatPercentage, string? note)
+    {
+        long createdWeighInId = -1;
+        DateTime createdWeighInDate = DateTime.MinValue;
+        float createdCanonicalPounds = 0;
+        float? createdBodyFatPercentage = null;
+        string? createdNote = null;
+
+        using (var connection = _sqliteConnectionFactory.CreateOpenConnection())
+        {
+            using (var createWeighInCommand = connection.CreateCommand())
+            {
+                createWeighInCommand.CommandText = "INSERT INTO weigh_in_entries (date, canonical_lbs, body_fat_percentage, note) VALUES " +
+                                                   "(@weighInDate, @weighInPounds, @weighInBodyFat, @weighInNote) " +
+                                                   "RETURNING id;";
+                
+                var dateParameter = new SqliteParameter("@weighInDate", creationDate.ToString("yyyy-MM-dd HH:mm:ss"));
+                var poundsParameter = new SqliteParameter("@weighInPounds", canonicalPounds);
+                var bodyFatParameter = new SqliteParameter("@weighInBodyFat", bodyFatPercentage != null ? bodyFatPercentage : DBNull.Value);
+                var noteParameter = new SqliteParameter("@weighInNote", note != null ? note : DBNull.Value);
+                createWeighInCommand.Parameters.AddRange(new List<SqliteParameter>()
+                {
+                    dateParameter,
+                    poundsParameter,
+                    bodyFatParameter,
+                    noteParameter
+                });
+                createWeighInCommand.Prepare();
+                
+                createdWeighInId = (long)createWeighInCommand.ExecuteScalar();
+            }
+
+            using (var selectCreatedWeighInCommand = connection.CreateCommand())
+            {
+                selectCreatedWeighInCommand.CommandText = "SELECT date, canonical_lbs, body_fat_percentage, note " +
+                                                          $"FROM weigh_in_entries WHERE id = {createdWeighInId} " +
+                                                          "LIMIT 1;";
+
+                using (var reader = selectCreatedWeighInCommand.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        createdWeighInDate = reader.GetDateTime(0);
+                        createdCanonicalPounds = reader.GetFloat(1);
+                        createdBodyFatPercentage = reader.IsDBNull(2) ? null : reader.GetFloat(2);
+                        createdNote = reader.IsDBNull(3) ? null : reader.GetString(3);
+                    }
+                }
+            }
+        }
+        
+        return new WeightJournalEntryDTO(createdWeighInId, createdWeighInDate, createdCanonicalPounds, createdBodyFatPercentage, createdNote);
     }
 
-    public WeightJournalEntryDTO UpdateExistingWeighIn(long id, DateTime creationDate, float canonicalPounds, float? bodyFatPercentage, string note)
+    public WeightJournalEntryDTO UpdateExistingWeighIn(long id, DateTime creationDate, float canonicalPounds, float? bodyFatPercentage, string? note)
     {
         throw new NotImplementedException();
     }
---
+++ Acai/AcaiCoreTests/WeightJournalGateway/WhenANewWeightJournalEntryIsCreated.cs
@@ -0,0 +1,65 @@
+﻿using AcaiCore;
+using NUnit.Framework;
+
+namespace AcaiCoreTests.WeightJournalGateway;
+
+public class WhenANewWeightJournalEntryIsCreated
+{
+    WeightJournalEntryDTO _result;
+    
+    [OneTimeSetUp]
+    public void Setup()
+    {
+        var connectionFactory = new TestingSqliteConnectionFactory();
+        using (var connection = connectionFactory.CreateOpenConnection())
+        {
+            using (var command = connection.CreateCommand())
+            {
+                foreach (var schema in AcaiCore.JournalTableSchemas.All)
+                {
+                    command.CommandText = schema.GetSQLTableCreationQuery();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+        
+        var subject = new AcaiCore.WeightJournalGateway(connectionFactory);
+        _result = subject.CreateNewWeighIn(new DateTime(2025, 12, 7, 18, 35, 32), 175, 30, "This is a test note.");
+    }
+
+    [Test]
+    public void ThenAnIdForTheNewEntryIsWritten()
+    {
+        Assert.That(_result.GetID(), Is.EqualTo(1));
+    }
+    
+    [Test]
+    public void ThenTheCorrectCreationDateIsWritten()
+    {
+        Assert.That(_result.GetCreationDate().Year, Is.EqualTo(2025));
+        Assert.That(_result.GetCreationDate().Month, Is.EqualTo(12));
+        Assert.That(_result.GetCreationDate().Day, Is.EqualTo(7));
+
+        Assert.That(_result.GetCreationDate().Hour, Is.EqualTo(18));
+        Assert.That(_result.GetCreationDate().Minute, Is.EqualTo(35));
+        Assert.That(_result.GetCreationDate().Second, Is.EqualTo(32));
+    }
+    
+    [Test]
+    public void ThenTheCorrectAmountOfCanonicalPoundsIsWritten()
+    {
+        Assert.That(_result.GetCanonicalPounds(), Is.EqualTo(175));
+    }
+    
+    [Test]
+    public void ThenTheCorrectBodyFatPercentageIsWritten()
+    {
+        Assert.That(_result.GetBodyFatPercentage(), Is.EqualTo(30));
+    }
+    
+    [Test]
+    public void ThenTheNoteIsWritten()
+    {
+        Assert.That(_result.GetNote(), Is.EqualTo("This is a test note."));
+    }
+}
