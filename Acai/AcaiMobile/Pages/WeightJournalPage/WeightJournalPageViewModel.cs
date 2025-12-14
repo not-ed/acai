11d71e50610c3c92d60464f87ad6e69f9dd304f3
not-ed <43544402+not-ed@users.noreply.github.com>
2025-12-14 20:24:55 +0000

    Corrected mapping and display of Weight Journal entries to WeightJournalPage

--- Acai/AcaiMobile/Pages/WeightJournalPage/WeightJournalPageViewModel.cs
+++ Acai/AcaiMobile/Pages/WeightJournalPage/WeightJournalPageViewModel.cs
@@ -1,15 +1,17 @@
 using System.Collections.ObjectModel;
+using AcaiCore;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
 namespace AcaiMobile;
 
-public partial class WeightJournalViewItem(DateTime creationDate, string displayWeight, float bodyFatPercentage) : ObservableObject
+public partial class WeightJournalViewItem(WeightJournalEntryDTO weighIn) : ObservableObject
 {
-    [ObservableProperty] private DateTime _creationDate = creationDate;
-    [ObservableProperty] private string _displayWeight = displayWeight;
-    [ObservableProperty] private float _bodyFatPercentage = bodyFatPercentage;
-    [ObservableProperty] private string _note = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec magna id lorem sollicitudin gravida. Integer molestie euismod arcu, vel.";
+    [ObservableProperty] private long _id = weighIn.GetID();
+    [ObservableProperty] private DateTime _creationDate = weighIn.GetCreationDate();
+    [ObservableProperty] private string _displayWeight = $"{weighIn.GetCanonicalPounds()}lbs";
+    [ObservableProperty] private float? _bodyFatPercentage = weighIn.GetBodyFatPercentage();
+    [ObservableProperty] private string _note = weighIn.GetNote();
     [ObservableProperty] private bool _isExpanded = false;
 }
 
@@ -27,14 +29,11 @@ public partial class WeightJournalPageViewModel : ObservableObject
     {
         EntryList.Clear();
         
-        EntryList.Add(new WeightJournalViewItem(DateTime.Now,"12st 9lbs", 30));
-        EntryList.Add(new WeightJournalViewItem(DateTime.Now.AddDays(-7),"80.2kg", 29.4f));
-        EntryList.Add(new WeightJournalViewItem(DateTime.Now.AddDays(-14),"177lbs", 29.6f));
-        EntryList.Add(new WeightJournalViewItem(DateTime.Now.AddDays(-21),"177lbs", 29.6f));
-        EntryList.Add(new WeightJournalViewItem(DateTime.Now.AddDays(-28),"177lbs", 29.6f));
-        EntryList.Add(new WeightJournalViewItem(DateTime.Now.AddDays(-35),"177lbs", 29.6f));
-        EntryList.Add(new WeightJournalViewItem(DateTime.Now.AddDays(-42),"177lbs", 29.6f));
-        EntryList.Add(new WeightJournalViewItem(DateTime.Now.AddDays(-49),"177lbs", 29.6f));
+        var session = await AcaiSessionSingleton.Get();
+        foreach (var weighIn in session.GetWeightJournalGateway().GetAllWeighIns())
+        {
+            EntryList.Add(new WeightJournalViewItem(weighIn));
+        }
     }
     
     [RelayCommand]
@@ -51,7 +50,17 @@ public partial class WeightJournalPageViewModel : ObservableObject
     [RelayCommand]
     public async void DisplayAndProcessWeighInCreation()
     {
-        var result = await Shell.Current.DisplayPromptAsync("STUB", "New Entry");
+        var session = await AcaiSessionSingleton.Get();
+        
+        //STUB
+        var r = new Random();
+        var lbs = 120 + r.NextSingle() % 45;
+        var bodyFat = r.NextSingle() % 35;
+        var ignoreFat = r.Next() % 2 == 0;
+        var ignoreNote = r.Next() % 2 == 0;
+        session.GetWeightJournalGateway().CreateNewWeighIn(DateTime.Now, lbs, ignoreFat ? null : bodyFat, ignoreNote ? null : $"Test note. IGNORE FAT: {ignoreFat}. IGNORE NOTE: {ignoreNote}");
+        
+        ReinitializeEntriesList();
     }
     
     [RelayCommand]
